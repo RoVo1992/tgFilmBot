b32f71b4d0c51146b88351bf0ffd4766f147909e
RoVo1992 <69080551+RoVo1992@users.noreply.github.com>
2022-03-03 20:34:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-TOKEN = '1993157310:AAF3yDlmJ_Qr9DlRoF36Y1O4ORDTEavFmvA'
+TOKEN = '#' 
 
 APP_NAME = 'enigmatic-earth-65773'
 
-config_for_db = {'host': 'j8oay8teq9xaycnm.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-                 'user': 'crlk7lhv0et43ide',
-                 'password': 'u98wzltt2n34f7os',
-                 'database': 'y9o3f7iuel3jl3jb'}
+config_for_db = {'host': '#',
+                 'user': '#',
+                 'password': '#',
+                 'database': '#'} 
 
 gd_folder_id = '1qjtpa0nGaxmepr9z1I_6X0Yp9M3hGvrs'
 
