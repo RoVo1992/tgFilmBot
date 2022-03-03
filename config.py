2d41306a9bed306c18a6b0653d1ba0525bdd88e6
RoVo1992 <69080551+RoVo1992@users.noreply.github.com>
2022-03-03 20:33:10 +0300

    2nd

---
+++ config.py
@@ -0,0 +1,12 @@
+TOKEN = '1993157310:AAF3yDlmJ_Qr9DlRoF36Y1O4ORDTEavFmvA'
+
+APP_NAME = 'enigmatic-earth-65773'
+
+config_for_db = {'host': 'j8oay8teq9xaycnm.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+                 'user': 'crlk7lhv0et43ide',
+                 'password': 'u98wzltt2n34f7os',
+                 'database': 'y9o3f7iuel3jl3jb'}
+
+gd_folder_id = '1qjtpa0nGaxmepr9z1I_6X0Yp9M3hGvrs'
+
+my_id = 447458089
